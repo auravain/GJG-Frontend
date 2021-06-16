a3352a56a52c81566cea8ef9c82102281d361ad4
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-17 00:45:31 +0300

    added table header

--- gjg-frontend/src/layouts/Dashboard.js
+++ gjg-frontend/src/layouts/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import ProductList from '../pages/ProductList';
 
 export default function Dashboard() {
 	return (
 		<div>
-			<Button style={{ marginTop: '0.5em' }}>Change Data</Button>
+			<Button style={{ marginTop: '0.5em', marginBottom: '0.7em' }}>Change Data</Button>
+			<ProductList />
 		</div>
 	);
 }
--- gjg-frontend/src/pages/ProductList.js
+++ gjg-frontend/src/pages/ProductList.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Table } from 'semantic-ui-react';
 
 export default function ProductList() {
-	return <div>
-		
-	</div>;
+	return (
+		<div>
+			<Table>
+				<Table.Header>
+					<Table.Row>
+						<Table.HeaderCell>App</Table.HeaderCell>
+						<Table.HeaderCell>Platform</Table.HeaderCell>
+						<Table.HeaderCell>Date</Table.HeaderCell>
+						<Table.HeaderCell>Impressions</Table.HeaderCell>
+						<Table.HeaderCell>Clicks</Table.HeaderCell>
+						<Table.HeaderCell>Installs</Table.HeaderCell>
+						<Table.HeaderCell>DAU</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body></Table.Body>
+			</Table>
+		</div>
+	);
 }
