36fb91aa1e5cd13851e02764891d8edb80a0524c
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-16 20:03:02 +0300

    created priority layers

---
+++ gjg-frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Dashboard from './layouts/Dashboard';
+import { Container } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Container className="main">
+				<Dashboard />
+			</Container>
+		</div>
+	);
+}
+
+export default App;
---
+++ gjg-frontend/src/layouts/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default function Dashboard() {
+	return (
+		<div>
+			<Button>Change Data</Button>
+		</div>
+	);
+}
