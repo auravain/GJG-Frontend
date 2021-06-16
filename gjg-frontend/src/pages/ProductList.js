75dee7b16f34e04e45781d610e9d738f787ceba9
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-17 01:58:15 +0300

    created default table

--- gjg-frontend/src/pages/ProductList.js
+++ gjg-frontend/src/pages/ProductList.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import { Table } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Table, Icon, Menu } from 'semantic-ui-react';
+import ProductService from '../services/productService';
 
 export default function ProductList() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		let productService = new ProductService();
+		productService.getProducts().then((result) => setProducts(result.data.data));
+	}, []);
+
 	return (
 		<div>
 			<Table>
@@ -16,7 +24,37 @@ export default function ProductList() {
 						<Table.HeaderCell>DAU</Table.HeaderCell>
 					</Table.Row>
 				</Table.Header>
-				<Table.Body></Table.Body>
+				<Table.Body>
+					{products.map((product) => (
+						<Table.Row>
+							<Table.Cell>{product.app}</Table.Cell>
+							<Table.Cell>{product.platform}</Table.Cell>
+							<Table.Cell>{product.date}</Table.Cell>
+							<Table.Cell>{product.impressions}</Table.Cell>
+							<Table.Cell>{product.clicks}</Table.Cell>
+							<Table.Cell>{product.installs}</Table.Cell>
+							<Table.Cell>{product.dau}</Table.Cell>
+						</Table.Row>
+					))}
+				</Table.Body>
+				<Table.Footer>
+					<Table.Row>
+						<Table.HeaderCell colSpan="3">
+							<Menu floated="right" pagination>
+								<Menu.Item as="a" icon>
+									<Icon name="chevron left" />
+								</Menu.Item>
+								<Menu.Item as="a">1</Menu.Item>
+								<Menu.Item as="a">2</Menu.Item>
+								<Menu.Item as="a">3</Menu.Item>
+								<Menu.Item as="a">4</Menu.Item>
+								<Menu.Item as="a" icon>
+									<Icon name="chevron right" />
+								</Menu.Item>
+							</Menu>
+						</Table.HeaderCell>
+					</Table.Row>
+				</Table.Footer>
 			</Table>
 		</div>
 	);
