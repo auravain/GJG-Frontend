9f3248c85186a0dd07528c4878257330b2d0790b
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-23 17:46:14 +0300

    set the button click event

--- gjg-frontend/src/layouts/Dashboard.js
+++ gjg-frontend/src/layouts/Dashboard.js
@@ -1,28 +1,21 @@
 import React, { useState } from 'react';
 import ProductList from '../pages/ProductList';
+import Graph from '../pages/Graph';
 import '../styles/dashboard.css';
 
 export default function Dashboard() {
-	//const [count, setCount] = useState(0);
-	//const [clicked, setClicked] = useState(false);
 	const [table, setTable] = useState(false);
+
 	const onClick = () => {
-		setTable(true);
+		setTable(!table);
 	};
-	/* 	function isClicked(e) {
-		e.preventDefault();
-		setCount(count + 1);
-		setClicked(true);
-		console.log('clicked');
-		setTable(true);
-	}
-	} */
+
 	return (
 		<div className="dash-style">
 			<button className="button" onClick={onClick}>
 				Change View of Data
 			</button>
-			{table ? <ProductList /> : null}
+			{table ? <ProductList /> : <Graph />}
 		</div>
 	);
 }
