090ebadfd4c73480e1576d771a8007fdc31c93dd
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-23 06:09:50 +0300

    imported styles into the App.js

--- gjg-frontend/src/App.js
+++ gjg-frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import Dashboard from './layouts/Dashboard';
 import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
