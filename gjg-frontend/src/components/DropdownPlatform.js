b722160c63d909ce7517ac1acb98492d4ab5c5a6
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-24 06:22:22 +0300

    platform filtering feature has been activated

--- gjg-frontend/src/components/DropdownPlatform.js
+++ gjg-frontend/src/components/DropdownPlatform.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 
-export default function DropdownPlatform() {
-	const [platformFilter, setPlatformFilter] = useState([]);
+export default function DropdownPlatform({ setPlatformFilter }) {
 	const options = [
 		{ key: 1, text: 'iOS', icon: 'apple', value: 1 },
 		{ key: 2, text: 'Android', icon: 'android', value: 2 },
@@ -15,9 +14,9 @@ export default function DropdownPlatform() {
 				options={options}
 				placeholder="Select a platform"
 				selection
-				style={{ fontSize: '15px', fontWeight: 'normal' }}
+				style={{ fontSize: '15px', fontWeight: 'normal', width: '12em' }}
 				onChange={(e) => {
-					setPlatformFilter(e);
+					setPlatformFilter(e.target.textContent);
 					console.log('selected' + e);
 				}}
 			/>
