a278e1989813ac806c8dff204694f006ef29005c
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-06-24 06:12:59 +0300

    created button component for sorting feature

---
+++ gjg-frontend/src/components/SortButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Icon } from 'semantic-ui-react';
+
+function SortButton() {
+	return (
+		<div>
+			<Button
+				style={{
+					border: 'none',
+					backgroundColor: '#f9fafb',
+					textAlign: 'center',
+					fontSize: '0.9em',
+					marginTop: '1em',
+					paddingRight: '1em',
+					paddingLeft: '1em',
+				}} /* options={options} */
+			>
+				<Icon id="icon" name="arrow up" style={{ fontSize: '1.5em' }} />
+				{/* {onClick ? 'arrow up' : 'arrow down'} */}
+			</Button>
+		</div>
+	);
+}
+
+export default SortButton;
